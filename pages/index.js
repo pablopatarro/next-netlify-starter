e458ecffa5e43d6b9ff89c090bba7fa4ea517355
pablopatarro <ico17rg@gmail.com>
2023-10-10 22:07:05 +0200

    Se ha modificado el fichero index.js

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,21 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className="container">
+            <Head>
+                <title>Esta es una prueba para ver si haciendo push desde VSCode se actualiza la página</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+            <main>
+                <Header title="Welcome to my app!" />
+                <p className="description">
+                    Vamos a tener que trabajar mucho para aprender Next.js...
+                </p>
+            </main>
 
-      <Footer />
-    </div>
-  )
+            <Footer />
+        </div>
+    )
 }
